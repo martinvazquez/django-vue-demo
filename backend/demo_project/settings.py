fa38267b3c6cd8c5892edea85269ad7573728d96
Martin Vazquez <martin@linisco.com.ar>
2025-07-30 20:14:12 -0300

    products -> cats

--- backend/demo_project/settings.py
+++ backend/demo_project/settings.py
@@ -24,8 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'products'
+    'django.contrib.staticfiles',    
+    'rest_framework',
+    'cats',
 ]
 
 MIDDLEWARE = [
