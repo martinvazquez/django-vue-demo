edbdf24678c4e0f70725c8efc60a885491fad98c
Martin Vazquez <martin@linisco.com.ar>
2025-07-30 20:24:19 -0300

    added cats api endpoint

--- backend/cats/views.py
+++ backend/cats/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cat
+from .serializers import CatSerializer
 
-# Create your views here.
+
+class CatViewSet(viewsets.ModelViewSet):    
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer    
